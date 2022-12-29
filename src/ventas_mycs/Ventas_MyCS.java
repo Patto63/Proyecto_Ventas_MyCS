551fe4ef3798037c12a42a6de48903a9ab4daba5
Patricio Salazar <psalazar1423@uta.edu.ec>
2022-12-28 21:34:01 -0500

    Inicializacion Proyecto

---
+++ src/ventas_mycs/Ventas_MyCS.java
@@ -0,0 +1,16 @@
+package ventas_mycs;
+
+/**
+ *
+ * @author Patto
+ */
+public class Ventas_MyCS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
